d3d5793158c3e61281ae77ce050c2cb93be68081
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-11-18 21:46:37 -0600

    Update supportserver.js

--- src/commands/info/supportserver.js
+++ src/commands/info/supportserver.js
@@ -1,1 +1,26 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
 
+module.exports = class SupportServerCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'supportserver',
+      aliases: ['support', 'ss'],
+      usage: 'supportserver',
+      description: 'Displays the invite link to Cryptic\'s Discord Support Server.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const embed = new MessageEmbed()
+      .setTitle('Support Server')
+      .setDescription('Click [here](https://discord.gg/DFZv2Zh) to join the Calypso Support Server!')
+      .addField('Other Links', 
+        '**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=612134650114080818&scope=bot&permissions=403008599) |**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
