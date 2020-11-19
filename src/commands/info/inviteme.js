6557a6d7559497247833dfb6d4ceefe82dca18b9
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-11-18 21:41:23 -0600

    Update inviteme.js

--- src/commands/info/inviteme.js
+++ src/commands/info/inviteme.js
@@ -1,1 +1,30 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const { oneLine } = require('common-tags');
 
+module.exports = class InviteMeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'inviteme',
+      aliases: ['invite', 'invme', 'im'],
+      usage: 'inviteme',
+      description: 'Generates a link you can use to invite Cryptic to your own server.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const embed = new MessageEmbed()
+      .setTitle('Invite Me')
+      .setDescription(oneLine`
+        Click [here](https://discordapp.com/oauth2/authorize?client_id=612134650114080818&scope=bot&permissions=403008599)
+        to invite me to your server!
+      `)
+      .addField('Other Links', 
+        '**[Support Server](https://discord.gg/DFZv2Zh) |**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
