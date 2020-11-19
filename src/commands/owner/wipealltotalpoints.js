c5adaf10628355c02b13fdc3c19d643a04fa5728
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-11-18 21:23:54 -0600

    Update wipealltotalpoints.js

--- src/commands/owner/wipealltotalpoints.js
+++ src/commands/owner/wipealltotalpoints.js
@@ -1,26 +1,30 @@
 const Command = require('../Command.js');
 const { MessageEmbed } = require('discord.js');
 
-module.exports = class WipeTotalPointsCommand extends Command {
+const rgx = /^(?:<@!?)?(\d+)>?$/;
+
+module.exports = class WipeAllTotalPointsCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'wipetotalpoints',
-      aliases: ['wipetp', 'wtp'],
-      usage: 'wipetotalpoints <user mention/ID>',
-      description: 'Wipes the provided user\'s points and total points.',
+      name: 'wipealltotalpoints',
+      aliases: ['wipeatp', 'watp'],
+      usage: 'wipealltotalpoints <server ID>',
+      description: 'Wipes all members\' points and total points in the server with the provided ID.',
       type: client.types.OWNER,
       ownerOnly: true,
-      examples: ['wipetotalpoints @++Roman++']
+      examples: ['wipealltotalpoints 709992782252474429']
     });
   }
   run(message, args) {
-    const member =  this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
-    if (!member)
-      return this.sendErrorMessage(message, 0, 'Please mention a user or provide a valid user ID');
-    message.client.db.users.wipeTotalPoints.run(member.id, message.guild.id);
+    const guildId = args[0];
+    if (!rgx.test(guildId))
+      return this.sendErrorMessage(message, 0, 'Please provide a valid server ID');
+    const guild = message.client.guilds.cache.get(guildId);
+    if (!guild) return this.sendErrorMessage(message, 0, 'Unable to find server, please check the provided ID');
+    message.client.db.users.wipeAllTotalPoints.run(guildId);
     const embed = new MessageEmbed()
-      .setTitle('Wipe Total Points')
-      .setDescription(`Successfully wiped ${member}'s points and total points.`)
+      .setTitle('Wipe All Total Points')
+      .setDescription(`Successfully wiped **${guild.name}**'s points and total points.`)
       .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setColor(message.guild.me.displayHexColor);
