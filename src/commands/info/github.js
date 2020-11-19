23b4962b80ee55a8dd675573b657805015b10c4a
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-11-18 21:38:45 -0600

    Update github.js

--- src/commands/info/github.js
+++ src/commands/info/github.js
@@ -1,1 +1,31 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const { oneLine } = require('common-tags');
 
+module.exports = class GitHubCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'github',
+      aliases: ['gh', 'repo'],
+      usage: 'github',
+      description: 'Displays the link to Cryptic\'s GitHub repository.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const embed = new MessageEmbed()
+      .setTitle('GitHub Link')
+      .setDescription(oneLine`
+        Click [here]() to to visit my GitHub repository!
+        Please support me by starring ⭐ the repo, and feel free to comment about issues or suggestions!
+      `)
+      .addField('Other Links',
+        '**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=612134650114080818&scope=bot&permissions=403008599) | ' +
+        '[Support Server](https://discord.gg/DFZv2Zh)**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
