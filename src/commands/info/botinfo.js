395095413230479821a2f1a2fe029c6280fb288c
romanbeard112 <62947856+romanbeard112@users.noreply.github.com>
2020-11-18 21:34:41 -0600

    Update botinfo.js

--- src/commands/info/botinfo.js
+++ src/commands/info/botinfo.js
@@ -1,1 +1,48 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const pkg = require(__basedir + '/package.json');
+const { owner } = require('../../utils/emojis.json');
+const { oneLine, stripIndent } = require('common-tags');
 
+module.exports = class BotInfoCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'botinfo',
+      aliases: ['bot', 'bi'],
+      usage: 'botinfo',
+      description: 'Fetches Cryptic\'s bot information.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const botOwner = message.client.users.cache.get(message.client.ownerId);
+    const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
+    const tech = stripIndent`
+      Version     :: ${pkg.version}
+      Library     :: Discord.js v12.3.1
+      Environment :: Node.js v12.16.3
+      Database    :: SQLite
+    `;
+    const embed = new MessageEmbed()
+      .setTitle('Cryptic\'s Bot Information')
+      .setDescription(oneLine`
+        Cryptic is a soon to be  open source, fully customizable Discord bot that is constantly growing.
+        Cryptic comes packaged with a variety of commands and 
+        a multitude of settings that can be tailored to your server's specific needs. 
+        Cryptics codebase also serves as a base framework to easily create Discord bots of all kinds.
+      `)
+      .addField('Prefix', `\`${prefix}\``, true)
+      .addField('Client ID', `\`${message.client.user.id}\``, true)
+      .addField(`Developer ${owner}`, botOwner, true)
+      .addField('Tech', `\`\`\`asciidoc\n${tech}\`\`\``)
+      .addField(
+        'Links', 
+        '**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=612134650114080818&scope=bot&permissions=403008599) | ' +
+        '[Support Server](https://discord.gg/DFZv2Zh) |**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
